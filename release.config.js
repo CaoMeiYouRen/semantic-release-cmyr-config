eab5df2283d58d6b044265a9fc9fce1d35cc344f
CaoMeiYouRen <caomeiyouren@qq.com>
2025-08-27 13:09:41 +0800

    fix: 更新依赖项并简化 release.config.js 配置

--- release.config.js
+++ release.config.js
@@ -1,39 +1,7 @@
-import pkg from './package.json' with { type: 'json' }
-const { name } = pkg
+// import pkg from './package.json' with { type: 'json' }
+// const { name } = pkg
+import config from './dist/index.mjs'
 /**
  * @type {import('semantic-release').GlobalConfig}
  */
-export default {
-    plugins: [
-        [
-            '@semantic-release/commit-analyzer',
-            {
-                config: 'conventional-changelog-cmyr-config',
-            },
-        ],
-        [
-            '@semantic-release/release-notes-generator',
-            {
-                config: 'conventional-changelog-cmyr-config',
-            },
-        ],
-        [
-            '@semantic-release/changelog',
-            {
-                changelogFile: 'CHANGELOG.md',
-                changelogTitle: `# ${name}`,
-            },
-        ],
-        '@semantic-release/npm',
-        '@semantic-release/github',
-        [
-            '@semantic-release/git',
-            {
-                assets: [
-                    'CHANGELOG.md',
-                    'package.json',
-                ],
-            },
-        ],
-    ],
-}
+export default config
